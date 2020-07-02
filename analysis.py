676a3f0444cf0e8e1eac144f6dfcdce1103b18e7
Vitalii Zavadskyi <zavadskiy.vs@phystech.edu>
2020-07-02 16:30:25 +0000

    Second version of analysis

--- analysis.py
+++ analysis.py
@@ -28,125 +28,129 @@ names = {"CLs": r'$CL_s$', "H0_DATA": r'$\chi^2_{H_0}(DATA)$',
         "H0_H1": r'$\chi^2_{H_0}(H_1)$',
         "H1_H0": r'$\chi^2_{H_1}(H_0)$'}
 
-def makeGrid(grid):
-    x = np.linspace(np.log10(grid[0]), np.log10(grid[1]), int(grid[2]))
-    if args.xl:
-        xInd = [np.where(x >= xl[0])[0].min(), np.where(x <= xl[1])[0].max() + 1]
-        x = x[x >= xl[0]]
-        x = x[x <= xl[1]]
-        print(xInd)
-    else:
-        xInd = []
-    y = np.linspace(np.log10(grid[3]), np.log10(grid[4]), int(grid[5]))
-    if args.yl:
-        yInd = [np.where(y >= yl[0])[0].min(), np.where(y <= yl[1])[0].max() + 1]
-        y = y[y >= yl[0]]
-        y = y[y <= yl[1]]
-        print(yInd)
-    else:
-        yInd = []
-    return x, y, xInd, yInd
 
+class DATA():
 
-def saver(k, ndim, name):
-    path = args.output
-    path = "{:02}_{}D_{}_{}".format(k, ndim, name, path)
-    plt.savefig(path, dpi=450)
-    print(path+" saved")
+    def __init__(self, CLs_map, grid):
+        self.CLs = CLs_map
+        self.grid = grid
+        self.axis = dict.fromkeys([0, 1])
+        self.axisInd = {0: [None, None], 1: [None, None]}
 
+    def _prepare(self):
+        for i in range(2):
+            self.makeAxis(i)
 
-def plot2D(x, y, data, name, title):
-    fig = plt.figure()
-    x = np.power(10, x)
-    y = np.power(10, y)
-    xx, yy = np.meshgrid(x, y)
-    cs = plt.contourf(xx, yy, data, cmap=cm.coolwarm)
-    plt.xlabel(r"$\sin^2 2\theta_{14}$")
-    plt.ylabel(r"$\Delta m^2_{41}$")
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.title(r"2D "+title+names[name])
-    plt.colorbar(cs)
-    plt.tight_layout()
+    def makeAxis(self, i):
+        grid = self.grid
+        Axis = np.linspace(np.log10(grid[3 * i]), 
+                np.log10(grid[3 * i + 1]), int(grid[3 * i + 2]))
+        for AxisLimits, j in enumerate([args.xl, args.yl]):
+            if j == i:
+                if AxisLimits:
+                    xl = np.log10(AxisLimits)
+                    xInd = [np.where(x >= xl[0])[0].min(), 
+                            np.where(x <= xl[1])[0].max() + 1]
+                    Axis = Axis[Axis >= xl[0]]
+                    Axis = Axis[Axis <= xl[1]]
+                    self.axisInd[i] = xInd
+        self.axis[i] = Axis.copy()
 
+    @classmethod
+    def saver(k, ndim, name):
+        path = args.output
+        path = "{:02}_{}D_{}_{}".format(k, ndim, name, path)
+        plt.savefig(path, dpi=450)
+        print(path+" saved")
 
-def plot3D(x, y, data, name, title):
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    xx, yy = np.meshgrid(x, y)
-    surf = ax.plot_surface(xx, yy, data, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-    ax.zaxis.set_major_locator(LinearLocator(10))
-    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-    ax.set_title(r"3D "+title+names[name])
-
-    plt.xlabel(r"$\log_{10} ( \sin^2 2\theta_{14}$ )")
-    plt.ylabel(r"$\log_{10} ( \Delta m^2_{41} )$")
-
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-    plt.tight_layout()
-
-
-def plotter(types, x, y, title, key0, key1=None, xind=None, yind=None):
-    if key1:
-        pre_data = CLs[key0]['map'] - CLs[key1]['map']
-    else:
-        pre_data = CLs[key0]['map']
-    if xind:
-        pre_data = pre_data[:,xind[0]:xind[1],:]
-    if yind:
-        pre_data = pre_data[yind[0]:yind[1],:,:]
-    for i, name in enumerate(types):
-        if args.log and i != 0:
-            data = np.log10(pre_data[:,:,i])
+    def _prepareData(self, name):
+        xInd, yInd = self.axisInd.values()
+        CLs = self.CLs
+        if args.log:
+            data = np.log10(CLs[yInd[0]:yInd[1], xInd[0]:xInd[1], data_type[name]])
         else:
-            data = pre_data[:,:,i]
+            data = (CLs[yInd[0]:yInd[1], xInd[0]:xInd[1], data_type[name]])
+        return data
+
+    def plot2D(self, name, title):
+        x, y = self.axis.values()
+        fig = plt.figure()
+        x, y = np.power(10, [x, y])
+        xx, yy = np.meshgrid(x, y)
+        data = self._prepareData(name)
+        cs = plt.contourf(xx, yy, data, cmap=cm.coolwarm)
+        plt.xlabel(r"$\sin^2 2\theta_{14}$")
+        plt.ylabel(r"$\Delta m^2_{41}$")
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.title(r"2D "+title+names[name])
+        plt.colorbar(cs)
+        plt.tight_layout()
+    
+    
+    def plot3D(self, name, title):
+        x, y = self.axis.values()
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        xx, yy = np.meshgrid(x, y)
+        data = self._prepareData(name)
+        surf = ax.plot_surface(xx, yy, data, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=False)
+        ax.zaxis.set_major_locator(LinearLocator(10))
+        ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+        ax.set_title(r"3D "+title+names[name])
+    
+        plt.xlabel(r"$\log_{10} ( \sin^2 2\theta_{14}$ )")
+        plt.ylabel(r"$\log_{10} ( \Delta m^2_{41} )$")
+    
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+        plt.tight_layout()
+
+
+    def plotter(self, name, title):
+        self._prepare()
         if args.D2:
-            plot2D(x, y, data, name, title)
+            self.plot2D(name, title)
             if args.output:
-                saver(i, 2, name)
+                self.saver(i, 2, name)
 
         if args.D3:
-            plot3D(x, y, data, name, title)
+            self.plot3D(name, title)
             if args.output:
-                saver(i, 3, name)
+                self.saver(i, 3, name)
+    
 
-CLs = dict()
-pathes = args.pathes
-
-if args.types:
-    types = args.types
-else:
-    types = ["CLs", "H0_DATA", "H1_DATA", "H0_H1", "H1_H0"]
-
-for path in args.pathes:
-    for filename in os.listdir(path):
-        if filename.endswith('.hdf5') and "CLs" in filename:
-            key = path.split('/')[-1]
-            CLs[key] = dict()
-            with h5py.File(path+'/'+filename, 'r') as f:
-                CLs[key]['map'] = f['CL\'s'][:]
-                CLs[key]['grid'] = f['grid'][:]
-
-if args.xl:
-    xl = np.log10(args.xl)
-if args.yl:
-    yl = np.log10(args.yl)
-
-if args.df:
-    key0 = args.pathes[0].split('/')[-1]
-    key1 = args.pathes[1].split('/')[-1]
-    x, y, xInd, yInd = makeGrid(CLs[key0]['grid'])
-
-    plotter(types, x, y, "Difference map of ", key0, key1, xInd, yInd)
-else:
+def main():
+    DATA_dict = dict()
+    pathes = args.pathes
+    
+    if args.types:
+        types = args.types
+    else:
+        types = ["CLs", "H1_DATA", "H0_H1", "H1_H0"]
+    
     for path in args.pathes:
-        key = path.split('/')[-1]
-        x, y, xInd, yInd = makeGrid(CLs[key]['grid'])
-       
-        plotter(types, x, y, "Map of ", key, xind=xInd, yind=yInd)
+        for filename in os.listdir(path):
+            if filename.endswith('.hdf5') and "CLs" in filename:
+                key = path.split('/')[-1]
+                with h5py.File(path+'/'+filename, 'r') as f:
+                    DATA_dict[key] = DATA(f['CL\'s'][:], f['grid'][:])
+   
+    if args.df:
+        key0, key1 = DATA_dict.keys()
+        DATA_df = DATA(DATA_dict[key0].CLs - DATA_dict[key1].CLs, DATA_dict[key0].grid)
+        for name in types:
+            DATA_df.plotter(name, "Difference map of ")
+    else:
+        for DATAs in DATA_dict.values():
+            for name in types:
+                DATAs.plotter(name, "Map of ")
+    
+    if args.show:
+        plt.show()   
+    
 
-if args.show:
-    plt.show()   
+if __name__ == '__main__':
+    main()
 
 
